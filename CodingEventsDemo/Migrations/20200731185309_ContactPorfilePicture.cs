f9eb901837ec567dc88f20e9891cd2f9289b74da
qsuelin <66089958+qsuelin@users.noreply.github.com>
2020-07-31 14:15:21 -0500

    add profile pic feature

---
+++ CodingEventsDemo/Migrations/20200731185309_ContactPorfilePicture.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingEventsDemo.Migrations
+{
+    public partial class ContactPorfilePicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "Contacts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Contacts");
+        }
+    }
+}
